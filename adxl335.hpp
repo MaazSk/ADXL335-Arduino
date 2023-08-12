fdf80f0b49d9001a61dbc8b62861c9ae1e4bf5bc
MaazSk <maazshaikh05@gmail.com>
2023-08-12 16:31:40 +0530

    First commit

---
+++ adxl335.hpp
@@ -0,0 +1,18 @@
+#ifndef _ADXL335_H
+#define _ADXL335_H
+#include <Arduino.h>
+class ADXL335{
+    public:
+    ADXL335(int pinX,int pinY, int pinZ);
+    void begin();
+    int getRawX();
+    int getRawY();
+    int getRawZ();
+    // void calibrateAccelerometer();
+    private:
+    int _pinX;
+    int _pinY;
+    int _pinZ;
+};
+
+#endif
